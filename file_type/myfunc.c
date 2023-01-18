70b7a6d644cd3f0dbb37cb77fb491a4fa549199b
Adward <1654783946@qq.com>
2023-01-18 20:28:00 +0800

    1.18

--- file_type/myfunc.c
+++ file_type/myfunc.c
@@ -3,6 +3,8 @@
 #include<unistd.h>
 int main(void)
 {
+    char *buf;
+    size_t size;
     if(chdir("/home/adward/桌面/Linux/Linux_new/one")<0)
         perror("chdir failed");
     printf("chdir to / temp succeeded\n");
